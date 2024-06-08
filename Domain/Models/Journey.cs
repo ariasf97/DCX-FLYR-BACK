d1f04127ab0afa1666b45765835aa90f3e2ed8ad
harrison arias <harias@emergiacc.com>
2024-06-07 19:49:33 -0500

    Journey model added

---
+++ Domain/Models/Journey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Journey
+    {
+        public Journey(List<Flight> flights, string origin, string destination, double priceJourney)
+        {
+            Flights = flights;
+            Origin = origin;
+            Destination = destination;
+            Price = priceJourney;
+        }
+
+
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public double Price { get; set; }
+
+        public List<Flight> Flights { get; set; }
+    }
+}
+
